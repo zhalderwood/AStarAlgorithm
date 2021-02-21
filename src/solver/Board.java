d1296c734275701a318034b5650d47241cd08147
zhalderwood <zju.0228@gmail.com>
2021-02-21 14:56:35 -0600

    Initial commit

---
+++ src/solver/Board.java
@@ -0,0 +1,5 @@
+package solver;
+
+public class Board {
+    
+}
---
+++ src/solver/Main.java
@@ -0,0 +1,8 @@
+package solver;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
