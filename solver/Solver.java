c432fd90be14004fcb03951b127210838022edbe
zhalderwood <60233767+zhalderwood@users.noreply.github.com>
2021-02-23 17:11:32 -0600

    Add files via upload

---
+++ solver/Solver.java
@@ -0,0 +1,18 @@
+package solver;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+import java.util.PriorityQueue;
+
+public class Solver {
+    private int searchDepth = 0;
+
+    public Solver() {
+
+    }
+
+    public int searchDepth() {
+        return 0;
+    }
+}
